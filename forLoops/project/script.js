167272a408d347e51b36ea7fde582ae8c3aaa564
erty1665 <eric.liz.16@wheels-student.org>
2016-03-15 19:32:52 +0000

    for loops

--- forLoops/project/script.js
+++ forLoops/project/script.js
@@ -1,2 +1,28 @@
 $( document ).ready(function() {
+//   
+   
+   for (var i = 0; i<100; i++){
+ //$('body').append('<img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Ursus_arctos_-_Norway.jpg" />');
+ 
+   } 
+   
+ 
+    
+    
+   /*
+   var i = 0;
+   
+   while(i < 100){
+      //add picture
+      
+      i++;
+   }
+   
+   
+   */
+    
+    
+    
+    
+    
 });
--- whileLoops/guided/app.js
+++ whileLoops/guided/app.js
@@ -1,8 +1,20 @@
 
 // How do I write a while loop that prints "Justin Bieber" 10 times?
-
+var count = 1;
+while(count < 11){
+    console.log("justin bieber!", count);
+    count++;
+}
 
 // How do I write a while loop that iterates 5 times and prints out the sequence 0 to 4?
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+}
 
 
 // How do I write a while loop that prints "Can't get enough of the Biebs" forever?
+while ("#"){
+    console.log("Can't get enough of the Biebs");
+}
